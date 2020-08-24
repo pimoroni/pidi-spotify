d112cd24a59f9b7f15099c72f98c1e9539088846
Phil Howard <phil@gadgetoid.com>
2020-08-24 16:36:02 +0100

    Tweaks to display output

--- pidi_spotify/st7789.py
+++ pidi_spotify/st7789.py
@@ -4,14 +4,14 @@ from . import display
 
 sys.modules["pidi.display"] = display
 import pidi_display_st7789
-from PIL import Image, ImageDraw
+from PIL import ImageDraw
 
 
 class DisplayST7789(pidi_display_st7789.DisplayST7789):
     def __init__(self, args=None):
         pidi_display_st7789.DisplayST7789.__init__(self, args)
 
-        # Remove the play/pause overlay controls, since there's no way to control transport via Raspotify
+        # Remove the next track overlay control, since there's no way to control transport via Raspotify
 
         controls_pause = ImageDraw.Draw(self.controls_pause)
         controls_pause.rectangle((205, 60, 235, 80), (0, 0, 0, 0))
